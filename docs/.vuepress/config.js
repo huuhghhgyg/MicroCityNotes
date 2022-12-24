8f4ada0f9b109ee4303231f64a3740ca7c32fd63
huuhghhgyg <huuhghhgyg@outlook.com>
2022-12-24 16:00:40 +0800

    设置网站图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,10 @@ module.exports = {
   base: "/MicroCityNotes/",
   title: "MicroCity笔记",
   description: "记录MicroCity的学习过程并提供快捷的参考",
-  head: [["meta", { name: "keywords", content: "MicroCity,笔记,文档" }]],
+  head: [
+    ["meta", { name: "keywords", content: "MicroCity,笔记,文档" }],
+    ["link", { rel: "icon", href: "/images/icon_microcity.png" }],
+  ],
   locales: {
     // 作为特例，默认语言可以使用 '/' 作为其路径。
     "/": {
