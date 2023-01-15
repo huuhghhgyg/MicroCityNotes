1431f2eb0ece3eb1a528c0e35dd63a32501b11fa
huuhghhgyg <huuhghhgyg@outlook.com>
2023-01-15 14:00:23 +0800

    使用布局槽添加主题
    取消修改模板

---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+        <!-- 评论 -->
+        <CommentService />
+    </template>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+</style>
