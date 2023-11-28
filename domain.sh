8e1064fa2543fb9110073104e7068bf8c5ff3e96
huuhghhgyg <huuhghhgyg@outlook.com>
2023-11-28 09:52:10 +0800

    [fix] robots.txt前缀修复

--- domain.sh
+++ domain.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # domain.sh 使用自定义域名需要修改的内容
+# Netlify 部署运行了这个脚本
 
 # 定义域名
 fixed_domain="huuhghhgyg.github.io"
@@ -7,6 +8,6 @@ domain="mcn.zhhuu.top"
 
 # 删除Base directory的设置
 sed -i '/base:/d' docs/.vuepress/config.js
-#使用定义的域名替换
+# 使用定义的域名替换
 sed -i "s/${fixed_domain}/${domain}/g" docs/.vuepress/config.js
-sed -i "s/${fixed_domain}/${domain}/g" docs/.vuepress/public/robots.txt
+sed -i "s/${fixed_domain}\/MicroCityNotes/${domain}/g" docs/.vuepress/public/robots.txt
